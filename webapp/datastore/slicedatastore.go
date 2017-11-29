8dfd570e6c9e445d6fb3d0226830621ebce592b6
Riccardo M. Cefala <riccardo.cefala@gmail.com>
2017-11-29 16:08:09 +0100

    webapp: split datastore interface from implementation

---
+++ webapp/datastore/slicedatastore.go
@@ -0,0 +1,49 @@
+package datastore
+
+//SliceDataStore is a Datastore that uses a slice as data store
+type SliceDataStore struct {
+	slice []Record
+}
+
+//NewSliceDataStore initializes a new slice based data store
+func NewSliceDataStore() *SliceDataStore {
+	return &SliceDataStore{}
+}
+
+//Init initializes the SliceDataStore with initial capacity initialCapacity
+func (d *SliceDataStore) Init(initialCapacity interface{}) error {
+	d.slice = make([]Record, initialCapacity.(int))
+	return nil
+}
+
+//Get all the elements in the SliceDataStore d
+func (d *SliceDataStore) Get() []Record {
+	return d.slice
+}
+
+//Add an element to the SliceDataStore d
+func (d *SliceDataStore) Add(record Record) {
+	for i, r := range d.slice {
+		if r.Key == record.Key {
+			d.slice[i].Value = record.Value
+			return
+		}
+	}
+
+	d.slice = append(d.slice, record)
+}
+
+//Rem ove an element from the SliceDataStore d
+func (d *SliceDataStore) Rem(record Record) {
+	for i, r := range d.slice {
+		if r.Key == record.Key {
+			d.slice[i] = d.slice[len(d.slice)-1]
+			d.slice = d.slice[:len(d.slice)-1]
+		}
+	}
+}
+
+//Size of the SliceDatastore d
+func (d *SliceDataStore) Size() int {
+	return len(d.slice)
+}
